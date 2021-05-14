1d439950d9a564a5bdc3bc36167ba29469ac3e47
caocao0103 <caocao0103@163.com>
2021-05-14 17:44:41 +0800

    新增

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <m-header></m-header>
+  <tab></tab>
+</template>
+
+<script>
+  import Header from '@/components/header/header'
+  import Tab from '@/components/tab/tab'
+
+  export default {
+    components: {
+      MHeader: Header,
+      Tab
+    }
+  }
+</script>
